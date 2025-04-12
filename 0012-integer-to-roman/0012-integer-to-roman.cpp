612dbe0b606f8c99f9be8e84007cd3629ebefae0
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-04-12 08:22:07 +0200

    Time: 8 ms (20.08%), Space: 17.1 MB (5.31%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string intToRoman(int num) 
+    {
+        map<int,string,greater<int>> um
+        {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+
+        string res = "";
+
+        for(auto &[value,symbol] : um)
+        {
+            while(num >= value)
+            {
+                res += symbol;
+                num -= value;
+            }
+        }   
+
+        return res;
+        
+    }
+};
