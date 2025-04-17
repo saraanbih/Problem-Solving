9ce0f7ac16f94410ab2ac74b36b9e45dbb112437
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-04-17 09:21:30 +0400

    Time: 0 ms (100%), Space: 8.7 MB (44.83%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    double help(double x,long long n){
-       if(n == 0) return 1;
-       double res = myPow(x*x,n/2);
-       if(n % 2)
-            res*=x;
-        return res;
-    }
     double myPow(double x, int n) {
-        long long l = n;
-        if(l < 0){
+        long long N = n;
+        if(N < 0){
             x = 1/x;
-            l=-l;
+            N = -N;
         }
-        return help(x,l);
+        if (N == 0) return 1;
+        double ans = myPow(x*x,N/2);
+        if(N%2) ans*=x;
+        return ans;
     }
 };
