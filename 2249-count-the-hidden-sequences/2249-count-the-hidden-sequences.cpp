8ecc71b5c3d86a638877894a0a50355ccbf99bfd
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-04-23 13:46:56 +0400

    Time: 0 ms (100%), Space: 112.6 MB (68.12%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
-        long mn = 0,mx = 0,sum =0;
-        for(auto &d : differences){
-            sum += d;
-            mn = min(mn,sum);
-            mx = max(mx,sum);
-        }
-        long mn_st = lower - mn;
-        long mx_st = upper - mx;
-        return max(0L,mx_st - mn_st + 1);
+       long sum =0,mn=0,mx=0;
+       for(auto&d : differences){
+          sum+=d;
+          mn = min(mn,sum);
+          mx = max(mx,sum);
+       }
+       long mn_st = lower - mn;
+       long mx_st = upper - mx;
+       return max(0L,mx_st-mn_st+1);
     }
 };
