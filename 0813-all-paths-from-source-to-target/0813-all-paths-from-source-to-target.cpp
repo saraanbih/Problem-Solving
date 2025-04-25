5d1e24892bd15b26f68702b22ebe144a37e910f5
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-04-25 22:39:40 +0400

    Time: 0 ms (100%), Space: 13.8 MB (77.26%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     void dfs(int node, vector<vector<int>>& graph, vector<int>& path, vector<vector<int>>& result) {
-        // Add the current node to the path
         path.push_back(node);
         
-        // If we've reached the last node (n-1), add the path to the result
         if (node == graph.size() - 1) {
             result.push_back(path);
         } else {
-            // Visit all neighbors
             for (int neighbor : graph[node]) {
                 dfs(neighbor, graph, path, result);
             }
         }
         
-        // Backtrack: remove the current node from the path
         path.pop_back();
     }
 
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> result;  // To store all the paths
-        vector<int> path;  // To store the current path
-        dfs(0, graph, path, result);  // Start DFS from node 0
+        vector<vector<int>> result;  
+        vector<int> path;  
+        dfs(0, graph, path, result); 
         return result;
     }
 };
