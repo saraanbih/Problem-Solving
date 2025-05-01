88ada27ddc88d35a6c3c27f603b11375ffaf4fa9
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-02 01:33:51 +0400

    Time: 852 ms (33.27%), Space: 285.8 MB (83.08%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
@@ -2,38 +2,25 @@ class Solution {
 public:
     int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills,
                       int strength) {
-        int n = tasks.size(), m = workers.size();
-        sort(tasks.begin(), tasks.end());
-        sort(workers.begin(), workers.end());
-
-        auto check = [&](int mid) -> bool {
+        int n = tasks.size(),m=workers.size();
+        sort(tasks.begin(),tasks.end());
+        sort(workers.begin(),workers.end());
+        auto check = [&](int md) -> bool {
             int p = pills;
-            // Ordered set of workers
             multiset<int> ws;
-            for (int i = m - mid; i < m; ++i) {
-                ws.insert(workers[i]);
-            }
-            // Enumerate each task from largest to smallest
-            for (int i = mid - 1; i >= 0; --i) {
-                // If the largest element in the ordered set is greater than or
-                // equal to tasks[i]
-                if (auto it = prev(ws.end()); *it >= tasks[i]) {
-                    ws.erase(it);
-                } else {
-                    if (!p) {
-                        return false;
-                    }
+            for(int i=m-md;i<m;i++)ws.insert(workers[i]);
+            for(int i=md-1;i>=0;i--){
+                if(auto it=prev(ws.end()); *it >= tasks[i])ws.erase(it);
+                else{
+                    if(!p)return false;
                     auto rep = ws.lower_bound(tasks[i] - strength);
-                    if (rep == ws.end()) {
-                        return false;
-                    }
+                    if(rep == ws.end())return false;
                     --p;
                     ws.erase(rep);
                 }
             }
             return true;
         };
-
         int left = 1, right = min(m, n), ans = 0;
         while (left <= right) {
             int mid = (left + right) / 2;
