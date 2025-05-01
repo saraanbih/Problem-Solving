b896e2699ba4c42501fe9cce93b2c02e0f3bc209
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-02 00:00:34 +0400

    Time: 4 ms (35.6%), Space: 12 MB (77.92%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node)return nullptr;
+        unordered_map<Node*,Node*> clone;
+        Node*copy = new Node(node->val);
+        clone[node] = copy;
+        queue<Node*> q;
+        q.push(node);
+        while(!q.empty()){
+            Node *cur = q.front();
+            q.pop();
+            for(auto &n : cur->neighbors){
+                if(!clone.count(n)){
+                    clone[n] = new Node(n->val);
+                    q.push(n);
+                }
+                clone[cur]->neighbors.push_back(clone[n]);
+            }
+        }   
+        return copy;
+    }
+};
