df62c9dc9e7663e68792b348630818ab2d8e46dd
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-02 18:09:51 +0400

    Time: 53 ms (79.36%), Space: 21.2 MB (63.12%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> st(wordList.begin(), wordList.end());
-        if (st.find(endWord) == st.end()) return 0;
-
-        queue<pair<string, int>> q;
-        q.push({beginWord, 1}); // (currentWord, depth)
-
-        while (!q.empty()) {
-            auto [word, steps] = q.front(); q.pop();
-
-            if (word == endWord) return steps;
-
-            st.erase(word);  // Mark as visited
-
-            for (int j = 0; j < word.size(); j++) {
-                char original = word[j];
-                for (int k = 0; k < 26; k++) {
-                    word[j] = 'a' + k;
-                    if (st.find(word) != st.end()) {
-                        q.push({word, steps + 1});
-                        st.erase(word); // Avoid revisiting
-                    }
+       unordered_set<string> st(wordList.begin(),wordList.end());
+       if(st.find(endWord) == st.end())return 0;
+       queue<pair<string,int>> q;
+       q.push({beginWord,1});
+       while(!q.empty()){
+          auto[word,deg] = q.front(); q.pop();
+          if(word == endWord) return deg;
+          st.erase(word);
+          for(int j = 0;j<word.size();j++){
+             char ch = word[j];
+             for(int i=0;i<26;i++){
+                word[j] = 'a' + i;
+                if(st.find(word) != st.end()){
+                    q.push({word,deg+1});
+                    st.erase(word);
                 }
-                word[j] = original;  // Restore
-            }
-        }
-
-        return 0;  // No path found
+             }
+             word[j] = ch;
+          }
+       }
+       return 0;
     }
 };
