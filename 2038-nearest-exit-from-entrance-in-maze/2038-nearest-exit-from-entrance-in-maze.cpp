d35c99572406b2b6eed87e000942865dc7ab5a4c
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-02 16:42:30 +0400

    Time: 4 ms (82.32%), Space: 36.1 MB (58.3%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    vector<int> dir = {0, 1, 0, -1, 0};
-
-    bool isValid(int x, int y, int r, int c) {
-        return (x >= 0 && x < r && y >= 0 && y < c);
+    vector<int> dir = {0,1,0,-1,0};
+    bool isValid(int x,int y,int r,int c){
+        return (x>=0 && x<r && y>=0 && y<c);
     }
-
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int r = maze.size(), c = maze[0].size();
-        queue<pair<int, int>> q;
-        vector<vector<bool>> vis(r, vector<bool>(c, false));
-        q.push({entrance[0], entrance[1]});
+        int r = maze.size(),c=maze[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<bool>> vis(r,vector<bool>(c,false));
+        q.push({entrance[0],entrance[1]});
         vis[entrance[0]][entrance[1]] = true;
-
         int res = 0;
-        while (!q.empty()) {
+        while(!q.empty()){
             int sz = q.size();
             res++;
-            while (sz--) {
-                auto [x, y] = q.front();
+            while(sz--){
+                auto[x,y] = q.front();
                 q.pop();
-                for (int i = 0; i < 4; i++) {
+                for(int i=0;i<4;i++){
                     int nx = x + dir[i];
-                    int ny = y + dir[i + 1];
-                    if (isValid(nx, ny, r, c) && maze[nx][ny] == '.' && !vis[nx][ny]) {
-                        if (nx == 0 || nx == r - 1 || ny == 0 || ny == c - 1)
-                            return res;
-                        q.push({nx, ny});
-                        vis[nx][ny] = true; // ✅ fix: mark as visited
+                    int ny = y + dir[i+1];
+                    if(isValid(nx,ny,r,c) && maze[nx][ny] == '.' && !vis[nx][ny]){
+                        if(nx == 0 || nx == r-1 || ny == 0 || ny == c-1) return res;
+                        q.push({nx,ny});
+                        vis[nx][ny] = true;
                     }
                 }
             }
