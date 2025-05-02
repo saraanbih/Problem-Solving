677dcc6d4c47d1af16753f6f50583633fdd6952e
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-02 18:32:37 +0400

    Time: 30 ms (27.32%), Space: 22.4 MB (31.76%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,54 +1,42 @@
 class Solution { 
 public:
-    vector<int> dir = {0, 1, 0, -1, 0};
-
-    bool isValid(int x, int y, int r, int c) {
+    vector<int> dir = {0,1,0,-1,0};
+    inline bool isValid(int x,int y,int r,int c){
         return (x >= 0 && x < r && y >= 0 && y < c);
     }
-
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int r = matrix.size(), c = matrix[0].size();
-        vector<vector<int>> indegree(r, vector<int>(c, 0));
-
-        // Step 1: Count in-degrees
-        for (int i = 0; i < r; ++i) {
-            for (int j = 0; j < c; ++j) {
-                for (int d = 0; d < 4; ++d) {
-                    int nx = i + dir[d];
-                    int ny = j + dir[d + 1];
-                    if (isValid(nx, ny, r, c) && matrix[nx][ny] > matrix[i][j])
-                        indegree[nx][ny]++;
-                }
+       int r = matrix.size(),c=matrix[0].size();
+       vector<vector<int>> indegree(r,vector<int>(c,0));
+       for(int i=0;i<r;i++){
+         for(int j=0;j<c;j++){
+            for(int d=0;d<4;d++){
+                int nx = i+dir[d];
+                int ny = j+dir[d+1];
+                if(isValid(nx,ny,r,c) && matrix[nx][ny] > matrix[i][j])indegree[nx][ny]++;
             }
+         }
+       }
+       queue<pair<int,int>> q;
+       for(int i=0;i<r;i++){
+         for(int j=0;j<c;j++){
+            if(indegree[i][j] == 0)q.push({i,j});
+         }
         }
-
-        // Step 2: Start from cells with indegree 0
-        queue<pair<int, int>> q;
-        for (int i = 0; i < r; ++i) {
-            for (int j = 0; j < c; ++j) {
-                if (indegree[i][j] == 0)
-                    q.emplace(i, j);
-            }
-        }
-
-        // Step 3: BFS - each level increases path length
         int res = 0;
-        while (!q.empty()) {
+        while(!q.empty()){
             int sz = q.size();
-            while (sz--) {
-                auto [x, y] = q.front(); q.pop();
-                for (int d = 0; d < 4; ++d) {
-                    int nx = x + dir[d];
-                    int ny = y + dir[d + 1];
-                    if (isValid(nx, ny, r, c) && matrix[nx][ny] > matrix[x][y]) {
-                        if (--indegree[nx][ny] == 0)
-                            q.emplace(nx, ny);
+            while(sz--){
+                auto[x,y] = q.front(); q.pop();
+                 for(int d=0;d<4;d++){
+                    int nx = x+dir[d];
+                    int ny = y+dir[d+1];
+                    if(isValid(nx,ny,r,c) && matrix[nx][ny] > matrix[x][y]){
+                        if(--indegree[nx][ny] == 0)q.emplace(nx,ny);
                     }
                 }
             }
             res++;
         }
-
         return res;
     }
 };
