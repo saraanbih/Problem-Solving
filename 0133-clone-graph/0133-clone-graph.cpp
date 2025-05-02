56fb69bb2cca89d0cbfbe51f131ac6df0664d941
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-02 17:00:58 +0400

    Time: 3 ms (68.54%), Space: 11.9 MB (89.36%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -22,23 +22,23 @@ public:
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        if(!node)return nullptr;
-        unordered_map<Node*,Node*> clone;
-        Node*copy = new Node(node->val);
-        clone[node] = copy;
-        queue<Node*> q;
-        q.push(node);
-        while(!q.empty()){
-            Node *cur = q.front();
-            q.pop();
-            for(auto &n : cur->neighbors){
-                if(clone.find(n) == clone.end()){
-                    clone[n] = new Node(n->val);
-                    q.push(n);
-                }
-                clone[cur]->neighbors.push_back(clone[n]);
-            }
-        }   
+       if(!node)return nullptr;
+       unordered_map<Node*,Node*> clone;
+       Node* copy = new Node(node->val);
+       clone[node] = copy;
+       queue<Node*> q;
+       q.push(node);
+       while(!q.empty()){
+          Node*cur = q.front();
+          q.pop();
+          for(auto &n : cur->neighbors){
+              if(clone.find(n) == clone.end()){
+                 clone[n] = new Node(n->val);
+                 q.push(n);
+              }
+              clone[cur]->neighbors.push_back(clone[n]);
+          }
+        }
         return copy;
     }
 };
