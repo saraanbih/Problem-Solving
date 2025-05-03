3d769e2e35cba1a4fe52dd6cfc01ee56f5db9daf
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-03 11:53:35 +0400

    Time: 0 ms (100%), Space: 115.5 MB (48.71%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        for(int i=1;i<=6;i++){
-            int rt = 0,rb=0;
-            bool can = true;
-            for(int j=0;j<tops.size();j++){
-                if(tops[j] != i && bottoms[j] != i){
-                    can = false;
-                    break;
-                }
-                if(tops[j] != i)rt++;
-                if(bottoms[j] != i)rb++;
-            }
-            if(can)return min(rt,rb);
-        }
-        return -1;
+       for(int i = 1;i<=6 ;i++){
+          int st = 0,sb = 0;
+          bool can_swap = true;
+          for(int j=0;j<tops.size();j++){
+              if(tops[j] != i && bottoms[j] != i){
+                 can_swap = false;
+                 break;
+              }
+              if(tops[j] != i)st++;
+              if(bottoms[j] != i)sb++;
+          }
+          if(can_swap) return min(st,sb);
+       }
+       return -1;
     }
 };
