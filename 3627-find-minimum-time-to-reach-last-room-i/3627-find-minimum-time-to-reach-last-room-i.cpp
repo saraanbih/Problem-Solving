a9cb7cbe98340a1866ef8b4f29bc164c0a6e1863
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-07 13:25:46 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    vector<int> dir = {0, 1, 0, -1, 0};
+    vector<int> dir = {0,1,0,-1,0};
 
-    bool isValid(int x, int y, int r, int c) {
-        return (x >= 0 && x < r && y >= 0 && y < c);
+    bool isValid(int x,int y,int r,int c){
+        return (x>=0 && x<r && y>=0 && y<c);
     }
 
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int n = moveTime.size(), m = moveTime[0].size();
-        vector<vector<int>> d(n, vector<int>(m, INT_MAX));
-        vector<vector<bool>> v(n, vector<bool>(m, false));
+        int r = moveTime.size(), c = moveTime[0].size();
+        vector<vector<int>> mn(r, vector<int>(c, INT_MAX));
+        vector<vector<bool>> vis(r, vector<bool>(c, false));
 
-        d[0][0] = 0;
+        using T = tuple<int,int,int>; // (time, x, y)
+        priority_queue<T, vector<T>, greater<T>> pq;
+        pq.push({0,0,0});
+        mn[0][0] = 0;
 
-        // (dis, x, y) - so priority_queue sorts by distance ascending
-        using T = tuple<int, int, int>;
-        priority_queue<T, vector<T>, greater<T>> q;
-        q.push({0, 0, 0}); // (distance, x, y)
-
-        while (!q.empty()) {
-            auto [dist, x, y] = q.top(); q.pop();
-            if (v[x][y]) continue;
-            v[x][y] = true;
+        while (!pq.empty()) {
+            auto [t, x, y] = pq.top(); pq.pop();
+            if (vis[x][y]) continue;
+            vis[x][y] = true;
 
             for (int i = 0; i < 4; i++) {
                 int nx = x + dir[i];
                 int ny = y + dir[i+1];
-                if (!isValid(nx, ny, n, m)) continue;
+                if (!isValid(nx, ny, r, c)) continue;
 
-                int newDist = max(d[x][y], moveTime[nx][ny]) + 1;
-                if (d[nx][ny] > newDist) {
-                    d[nx][ny] = newDist;
-                    q.push({newDist, nx, ny});
+                int nt = max(mn[x][y], moveTime[nx][ny]);
+                if (nt < mn[nx][ny]) {
+                    mn[nx][ny] = nt;
+                    pq.push({nt, nx, ny});
                 }
             }
         }
 
-        return d[n - 1][m - 1];
+        return mn[r-1][c-1];
     }
 };
