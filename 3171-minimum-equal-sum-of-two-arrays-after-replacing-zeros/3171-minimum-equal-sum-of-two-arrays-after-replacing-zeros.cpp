33a50625223ab6c0c8811cf2b3ee420a72db5187
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-10 10:05:35 +0400

    Time: 124 ms (81.69%), Space: 135.3 MB (93.56%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
-        long long s1 = std::accumulate(nums1.begin(),nums1.end(),0LL);
-        long long s2 = std::accumulate(nums2.begin(),nums2.end(),0LL);
-        int z1 = count(nums1.begin(),nums1.end(),0);
-        int z2 = count(nums2.begin(),nums2.end(),0);
-        s1+=z1;
-        s2+=z2;
-        if(!z1 && s1<s2 || !z2 && s2<s1)return -1;
-        return max(s1,s2);
+       long long s1 = std::accumulate(nums1.begin(),nums1.end(),0LL);
+       long long s2 = std::accumulate(nums2.begin(),nums2.end(),0LL);
+       int z1 = count(nums1.begin(),nums1.end(),0);
+       int z2 = count(nums2.begin(),nums2.end(),0);
+       s1+=z1;
+       s2+=z2;
+       if(!z1 && s2>s1 || !z2 && s1>s2)return -1;
+       return max(s1,s2);
     }
 };
