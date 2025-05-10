e8cd417396b87669f13919c2b3cbc0d007a65d0c
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-10 09:15:32 +0400

    Time: 136 ms (37.29%), Space: 135.5 MB (50.17%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -5,9 +5,9 @@ public:
         long long s2 = std::accumulate(nums2.begin(),nums2.end(),0LL);
         int z1 = count(nums1.begin(),nums1.end(),0);
         int z2 = count(nums2.begin(),nums2.end(),0);
-        s1 += z1;
-        s2 += z2;
-        if(!z1 && s2 > s1 || !z2 && s1 > s2)return -1;
+        s1+=z1;
+        s2+=z2;
+        if(!z1 && s1<s2 || !z2 && s2<s1)return -1;
         return max(s1,s2);
     }
 };
