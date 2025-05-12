0bd7f09e3127464b533413133a4d97a29fcf338d
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-12 07:47:54 +0400

    Time: 4 ms (71.67%), Space: 12.5 MB (93.09%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
-        int n = digits.size();
-        set<int> st;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                if(i == j)continue;
-                for(int k = 0;k<n;k++){
-                    if(k == i || k == j)continue;
-                    int num = (digits[i]*10 + digits[j])*10 + digits[k];
-                    if((num/100 > 0) && (num%2 == 0))st.insert(num);
+        bool used[10] = {false};
+        for (int d : digits) used[d] = true;
+
+        vector<int> result;
+
+        // Try all 3-digit numbers from 100 to 998 (step by 2 to ensure even)
+        for (int num = 100; num <= 998; num += 2) {
+            int a = num / 100;
+            int b = (num / 10) % 10;
+            int c = num % 10;
+
+            // Count how many times each digit appears in num
+            int freq[10] = {0};
+            freq[a]++;
+            freq[b]++;
+            freq[c]++;
+
+            bool canForm = true;
+            int available[10] = {0};
+            for (int d : digits) available[d]++;
+
+            for (int d = 0; d <= 9; ++d) {
+                if (freq[d] > available[d]) {
+                    canForm = false;
+                    break;
                 }
             }
+
+            if (canForm)
+                result.push_back(num);
         }
-        vector<int> res;
-        for(auto &ele:st)res.push_back(ele);
-        return res;
+
+        return result;
     }
 };
