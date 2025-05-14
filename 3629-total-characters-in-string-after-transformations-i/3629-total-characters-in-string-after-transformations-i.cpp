e2cf0355b06f81317df2ae32e3f864d16a1bd1c0
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-14 07:38:33 +0400

    Time: 827 ms (38.54%), Space: 495 MB (13.66%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int MOD = 1e9 + 7;
     int lengthAfterTransformations(string s, int t) {
-        vector<int> cnt(26);
-        for(auto &ch : s)cnt[ch-'a']++;
+        vector<int> freq(26,0);
+        for(auto &ch : s)freq[ch-'a']++;
         while(t--){
-            vector<int> nxt(26);
-            nxt[0] = cnt[25];
-            nxt[1] = (cnt[0] + cnt[25])%MOD;
-            for(int i=2;i<26;i++)
-                nxt[i] = cnt[i-1];
-            cnt = move(nxt);
+            vector<int> nxt(26,0);
+            nxt[0] = freq[25]; 
+            nxt[1] = (freq[25] + freq[0]) % MOD;
+            for(int i = 2;i<26;i++){
+                nxt[i] = freq[i-1];
+            }
+            freq = nxt;
         }
         int ans = 0;
-        for(int i = 0;i < 26 ; i++){
-            ans = (ans + cnt[i]) % MOD;
-        }
+        for(int i = 0;i<26;i++)
+            ans = (ans + freq[i])%MOD;
         return ans;
     }
 };
