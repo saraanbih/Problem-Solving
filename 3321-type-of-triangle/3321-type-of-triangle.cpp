30047d303f5af14b9596317ee89d7200483e3f76
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-19 12:23:29 +0400

    Time: 0 ms (100%), Space: 22.8 MB (65.15%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.cpp
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     string triangleType(vector<int>& nums) {
         int n1 = nums[0],n2=nums[1],n3=nums[2];
-        cout << n1 << " " << n2 << " " << n3;
-        if((n1+n2<=n3) || (n2+n3<=n1) || (n1+n3<=n2))return "none";
-        int unique = 0;
-        if((n1 != n2) && (n1!=n3))unique++;
-        if((n2!=n1) && (n2!=n3))unique++;
-        if((n3!=n1) && (n3!=n2))unique++;
-        if(unique == 0)return "equilateral";
-        if(unique == 1)return "isosceles";
-        return "scalene";
+        if((n1+n2>n3) && (n2+n3>n1) && (n1+n3>n2)){
+            if((n1 == n2) && (n2 == n3))return "equilateral";
+            if((n1 == n2) || (n1 == n3) || (n2 == n3))return "isosceles";
+            return "scalene";
+        }        
+        return "none";
     }
 };
