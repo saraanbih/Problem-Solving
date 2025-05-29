c053351564453ae6660f61d39109983027524f06
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-05-29 09:17:59 +0300

    Time: 112 ms (88.89%), Space: 59.8 MB (83.33%) - LeetHub

--- 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int dfs(int node,int parent,vector<vector<int>> &adj,int k){
         if(k<0)return 0;
-        int res = 1;
-        for(auto&ne : adj[node]){
-            if(ne == parent)continue;
-            res += dfs(ne,node,adj,k-1);
+        int res=1;
+        for(auto&n : adj[node]){
+            if(n == parent)continue;
+            res += dfs(n,node,adj,k-1);
         }
         return res;
     }
-    vector<int> build(vector<vector<int>> &edges,int k){
+    vector<int> build(vector<vector<int>>&edges,int k){
         int n = edges.size()+1;
         vector<vector<int>> adj(n);
-        for(auto &ed:edges){
+        for(auto& ed:edges){
             adj[ed[0]].push_back(ed[1]);
             adj[ed[1]].push_back(ed[0]);
         }
@@ -25,12 +25,10 @@ public:
         int n = edges1.size()+1,m=edges2.size()+1;
         vector<int> cnt1 = build(edges1,k);
         vector<int> cnt2 = build(edges2,k-1);
-        int mx_cnt = *max_element(cnt2.begin(),cnt2.end());
         vector<int> res(n);
-        for(int i = 0;i<n;i++){
+        int mx_cnt = *max_element(cnt2.begin(),cnt2.end());
+        for(int i=0;i<n;i++)
             res[i] = mx_cnt + cnt1[i];
-        }
         return res;
-
     }
 };
