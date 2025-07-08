fa8610771b6ce8c7067359839802b22ebdecb196
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-07-08 11:54:25 +0300

    Time: 65 ms (43.36%), Space: 75 MB (64.45%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-        // Sort events by their start day
-        sort(events.begin(), events.end());
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        int n = events.size();
-        int res = 0, day = 0, i = 0;
-        
-        // Loop until all events are processed and the heap is empty
-        while (i < n || !minHeap.empty()) {
-            // If no events are available, jump to next event's start day
-            if (minHeap.empty())
-                day = events[i][0];
-            // Add all events starting today to the heap
-            while (i < n && events[i][0] == day)
-                minHeap.push(events[i++][1]);
-            // Attend the event that ends earliest
-            minHeap.pop();
-            res++;
-            day++;
-            // Remove events that have already ended
-            while (!minHeap.empty() && minHeap.top() < day)
-                minHeap.pop();
-        }
-        return res;
+       sort(events.begin(),events.end());
+       priority_queue<int,vector<int>,greater<int>>pq;
+       int res = 0,day = 0,i=0;
+       int n = events.size();
+       while(i < n || !pq.empty()){
+          if(pq.empty())
+            day = events[i][0];
+          while(i < n && events[i][0] == day)
+            pq.push(events[i++][1]);
+          pq.pop();
+          res++;
+          day++;
+          while(!pq.empty() && pq.top() < day)
+            pq.pop();
+       }
+       return res;
     }
 };
