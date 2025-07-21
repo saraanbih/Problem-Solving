b41395aaed1fd8ee004a680814af422e4fad0fa1
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-07-21 05:44:40 +0300

    Time: 11 ms (75%), Space: 49.3 MB (63.33%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cs
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public string MakeFancyString(string s) {
         StringBuilder ans = new StringBuilder();
-        int freq = 1;
         ans.Append(s[0]);
         for(int i=1;i<s.Length;i++){
-            if(s[i]==s[i-1])freq++;
-            else freq = 1;
-            if(freq > 2)continue;
+            int len = ans.Length;
+            if(len >= 2 && s[i] == s[i-1] && s[i] == s[i-2])
+                continue;
             ans.Append(s[i]);
         }
         return ans.ToString();
