9e29c6eaf2fb39a9527e906a50dad477bb74498e
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-09-05 09:08:29 +0300

    Time: 0 ms (100%), Space: 8.2 MB (85.95%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
@@ -3,11 +3,11 @@ public:
     int makeTheIntegerZero(int num1, int num2) {
         int k = 1;
         while(true){
-            long long x = num1 - static_cast<long long>(num2)*k;
+            long long x = num1 - static_cast<long long>(num2) * k;
             if(x < k)return -1;
             if(k >= __builtin_popcountll(x))return k;
-            k++;
-        }   
-        return k;
+            k++;   
+        }
+        return -1;
     }
 };
