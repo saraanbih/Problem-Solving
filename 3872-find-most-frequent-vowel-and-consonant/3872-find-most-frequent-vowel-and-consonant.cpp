cc5f6927323b764fe1a0d58f3b7289a1003a93bc
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-09-13 14:07:48 +0300

    Time: 4 ms (19.67%), Space: 9.8 MB (44.54%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        unordered_map<char,int> cnt;
+        int cnt[26] = {0};
         unordered_set<char> vowels = {'a','e','i','o','u'};
         int cnt_v = 0, cnt_c = 0;
         for(auto &ch : s){
-            if(vowels.count(ch)){
-                cnt[ch]++;
-                cnt_v = max(cnt_v,cnt[ch]);
-            }
-            else{
-                cnt[ch]++;
-                cnt_c = max(cnt_c,cnt[ch]);
-            }
+            int idx = ch - 'a';
+            cnt[idx]++;
+            if(vowels.count(ch))
+                cnt_v = max(cnt_v,cnt[idx]);
+            else
+                cnt_c = max(cnt_c,cnt[idx]);
         }
         return cnt_v + cnt_c;
     }
