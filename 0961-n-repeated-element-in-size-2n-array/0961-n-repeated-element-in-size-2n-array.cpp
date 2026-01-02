5834c25345737b4e7073f3cc74c830f9b2d31681
Sara Nabih <nabihsara8@gmail.com>
2026-01-02 07:32:12 +0200

    Time: 21 ms (19.31%), Space: 34.1 MB (16.71%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        int n = nums.size()/2;
+        unordered_map<int,int> freq;
+        for(auto &num : nums){
+            freq[num]++;
+            if(freq[num] >= n)return num;
+        }
+        return 0;
+    }
+};
